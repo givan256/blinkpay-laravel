ff8d8bfc77b0443978c045a0c50fa6df99c14264
givan256 <c0d3d101.inc@gmail.com>
2025-05-23 03:43:36 +0300

    feat: add mobileMoney method
    - Add mobileMoney method as alias for processPayment
    - Add currency conversion support
    - Add proper type hints and documentation

--- src/BlinkPayGateway.php
+++ src/BlinkPayGateway.php
@@ -5,16 +5,26 @@ namespace BlinkPay\Laravel;
 class BlinkPayGateway
 {
     protected $service;
-    protected $convertToUGX;
+    protected $convertToUgx;
     
-    public function __construct(BlinkPayService $service, bool $convertToUGX = false)
+    public function __construct(BlinkPayService $service, bool $convertToUgx = false)
     {
         $this->service = $service;
-        $this->convertToUGX = $convertToUGX;
+        $this->convertToUgx = $convertToUgx;
     }
     
-    public function processPayment($orderData)
+    public function mobileMoney(array $data)
     {
+        return $this->processPayment($data);
+    }
+    
+    public function processPayment(array $orderData)
+    {
+        if ($this->convertToUgx) {
+            $orderData['amount'] = $this->convertToUgx($orderData['amount'], $orderData['currency'] ?? 'USD');
+            $orderData['currency'] = 'UGX';
+        }
+        
         $amount = $orderData['amount'];
         $phoneNumber = $this->service->validateMsisdn($orderData['phone_number']);
         
@@ -22,24 +32,34 @@ class BlinkPayGateway
             throw new \Exception('Invalid phone number format');
         }
         
-        if ($this->convertToUGX) {
-            $exchangeRate = $this->service->getForex();
-            $amount = $exchangeRate * $amount;
-        }
-        
         $description = "Order #{$orderData['order_id']} - Amount: {$orderData['currency']}{$orderData['amount']}";
         
         return $this->service->makePayment($phoneNumber, $amount, $description);
     }
     
-
     public function processCreditCardPayment(array $data)
     {
+        if ($this->convertToUgx) {
+            $data['amount'] = $this->convertToUgx($data['amount'], $data['currency'] ?? 'USD');
+            $data['currency'] = 'UGX';
+        }
+        
         try {
             $result = $this->service->processCreditCardPayment($data);
             return $result;
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
+    }
+    
+    protected function convertToUgx($amount, $currency)
+    {
+        // If already in UGX, return as is
+        if ($currency === 'UGX') {
+            return $amount;
+        }
+        
+        // Convert to UGX using the default exchange rate
+        return $amount * config('blinkpay.default_exchange_rate', 3700);
     }
 } 
